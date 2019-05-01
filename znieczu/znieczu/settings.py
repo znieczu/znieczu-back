66d2e3672504b601f3cbbafdffda87101cf6a821
znieczu <znieczu.tech.assistenza@gmail.com>
2019-05-01 12:04:59 +0200

    fixed auth login

--- znieczu/znieczu/settings.py
+++ znieczu/znieczu/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
     # Our custom apps
     'apps.company_walfare',
     
--- znieczu/znieczu/urls.py
+++ znieczu/znieczu/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('rest_auth.urls')),
     path('api/', include('api.api_company_walfare.urls')
          )
 ]
