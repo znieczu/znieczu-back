9fae7650fac72c6692e6666d9e37a9d33bb89134
znieczu <znieczu.tech.assistenza@gmail.com>
2019-05-07 22:50:35 +0200

    db docker setup

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 cd znieczu
 python manage.py collectstatic --no-input
 python manage.py makemigrations
+python manage.py makemigrations --merge --noinput
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
